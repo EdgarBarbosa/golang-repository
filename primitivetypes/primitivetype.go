221f0857be4cdc0b398c53a43a4a5651adb6640c
Edgar de Oliveira <edgar.oliveira@ebinterchange.com.br>
2025-08-10 14:12:40 -0300

    varibles

---
+++ primitivetypes/primitivetype.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// key word declaring
+	var myVariable string
+	fmt.Println(myVariable)
+
+	var myOtherVariable = "value for my other varible"
+	fmt.Println(myOtherVariable)
+
+	// short declation
+
+	myShortDeclationVariable := "value for my another varible"
+
+	fmt.Println(myShortDeclationVariable)
+}
